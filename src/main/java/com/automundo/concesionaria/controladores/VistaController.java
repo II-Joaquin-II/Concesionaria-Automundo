f807bab0d3ba98f93d93203b449870b8950a9606
AnthonyYuvaniTorresIzaguirre <u22330304@utp.edu.pe>
2025-05-12 18:16:28 -0500

    Agregue vistas para admin y sus botones principales, animación a los botones y responsive para cada uno.

--- src/main/java/com/automundo/concesionaria/controladores/VistaController.java
+++ src/main/java/com/automundo/concesionaria/controladores/VistaController.java
@@ -11,5 +11,28 @@ public class VistaController {
     public String cargarVistaAdmin() {
         return "admin";
     }
+    
+    @GetMapping("/adminverclientes")
+    public String cargarVistaVerClientes() {
+        return "adminverclientes";
+    }
+    
+    @GetMapping("/adminverautos")
+    public String cargarVistaVerAutos() {
+        return "adminverautos";
+    }
+    
+     @GetMapping("/adminverreclamos")
+    public String cargarVistaVerReclamos() {
+        return "adminverreclamos";
+    }
+     @GetMapping("/adminventaauto")
+    public String cargarVistaVentaAuto() {
+        return "adminventaauto";
+    }
+     @GetMapping("/adminventaautoalquilado")
+    public String cargarVistaVentaAutoAlquilado() {
+        return "adminventaautoalquilado";
+    }
 
 }
