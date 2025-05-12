006104934155084b86b0c3702504b1ecabc88d4b
II-Joaquin-II <U22221498@utp.edu.pe>
2025-05-11 19:14:05 -0500

    Implementación de botones Buscar, Actualizar, Eliminar e Insertar en vista admin del cliente.

--- src/main/java/com/automundo/concesionaria/servicios/ClientesService.java
+++ src/main/java/com/automundo/concesionaria/servicios/ClientesService.java
@@ -1,6 +1,7 @@
 package com.automundo.concesionaria.servicios;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.automundo.concesionaria.model.Clientes;
@@ -18,6 +19,23 @@ public class ClientesService {
         return repo_clientes.findAll();
     }
 
+    public Optional<Clientes> BuscaId(int id_cli) {
+        return repo_clientes.findById(id_cli);
+    }
+
+    public Clientes Actualizar(int id_cli, Clientes actualiza) {
+        Clientes clienteExiste = repo_clientes.findById(id_cli)
+        .orElseThrow(() -> new RuntimeException("Cliente no existe" + id_cli));
+        return repo_clientes.save(clienteExiste);
+    }
+
+    public Clientes Guardar(Clientes cliente) {
+        return repo_clientes.save(cliente);
+    }
+
+    public void eliminar(int id_cli) {
+        repo_clientes.deleteById(id_cli);
+    }
 
 }
     
