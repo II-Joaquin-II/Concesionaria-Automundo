a144d94d42849e38772fd688da49f74f43a84d59
AnghelloVT <u22325555@utp.edu.pe>
2025-06-30 16:22:34 -0500

    Descargar listado de accesorios en pdf y excel

--- src/main/java/com/automundo/concesionaria/controladores/AccesorioController.java
+++ src/main/java/com/automundo/concesionaria/controladores/AccesorioController.java
@@ -1,19 +1,40 @@
 package com.automundo.concesionaria.controladores;
 
 import com.automundo.concesionaria.model.Accesorio;
+import com.automundo.concesionaria.repositorio.AccesorioRepositorio;
 import com.automundo.concesionaria.servicios.AccesorioServicio;
+import com.automundo.concesionaria.util.ListarAccesoriosExcel;
+import com.automundo.concesionaria.util.ListarAccesoriosPDF;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class AccesorioController {
+    
+    private final ListarAccesoriosExcel listarAccesoriosExcel;
+    
+    @Autowired
+    private AccesorioRepositorio accesorioRepositorio;
+
 
     @Autowired
     private AccesorioServicio servicio;
+     @Autowired
+    private ListarAccesoriosPDF listarAccesoriosPDF;
+
+    public AccesorioController(ListarAccesoriosExcel listarAccesoriosExcel) {
+        this.listarAccesoriosExcel = listarAccesoriosExcel;
+    }
 
     @GetMapping("/admin-accesorios")
     public String verPaginaAdmin(Model modelo) {
@@ -55,11 +76,29 @@ public class AccesorioController {
         return "admin-accesorios";
     }
 
+    @GetMapping("/descargar-excel")
+    public ModelAndView descargarExcel() {
+        List<Accesorio> accesorios = accesorioRepositorio.findAll();
+        Map<String, Object> model = new HashMap<>();
+        model.put("accesorios", accesorios);
+        return new ModelAndView(listarAccesoriosExcel, model);
+    }
+
+    @GetMapping("/descargar-pdf")
+    public void descargarReportePDF(HttpServletResponse response) throws Exception {
+        List<Accesorio> accesorios = accesorioRepositorio.findAll();
+
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=listado-accesorios.pdf");
+
+        listarAccesoriosPDF.exportarPDF(accesorios, response.getOutputStream());
+    }
+
     @GetMapping("/vista-accesorios.html")
     public String vistaAccesorios(Model model) {
         List<Accesorio> accesorios = servicio.listarTodos();
         model.addAttribute("accesorios", accesorios);
-        return "vista-accesorios"; 
+        return "vista-accesorios";
     }
 
 }
--- src/main/java/com/automundo/concesionaria/repositorio/AccesorioRepositorio.java
+++ src/main/java/com/automundo/concesionaria/repositorio/AccesorioRepositorio.java
@@ -2,8 +2,10 @@
 package com.automundo.concesionaria.repositorio;
 
 import com.automundo.concesionaria.model.Accesorio;
+import com.automundo.concesionaria.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface AccesorioRepositorio extends JpaRepository<Accesorio, Long> {
     List<Accesorio> findByNombreContainingIgnoreCase(String nombre);
